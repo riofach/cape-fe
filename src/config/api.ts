cd5aef23be97b82ec984659d9920cfe57c62e710
riofach <fachrio.raditya@gmail.com>
2025-04-25 20:13:12 +0700

    fixed api railway

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 // Untuk development:
-export const API_BASE_URL = 'http://localhost:5000/api';
+// export const API_BASE_URL = 'http://localhost:5000/api';
 
 // Untuk production:
-// export const API_BASE_URL = 'https://cape-be.up.railway.app/api';
+export const API_BASE_URL = 'https://cape-be.up.railway.app/api';
